21e961819fad35ca4c41b5c5bc3b9d2477581c79
bakyvelmammedov <bakyvelmammedov@gmail.com>
2021-02-04 17:45:48 +0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        String[] words = {input.nextLine(),input.nextLine(),input.nextLine(),input.nextLine(),input.nextLine()};
+
+
+
+        int i = 0;
+        String[] newWords = new String[words.length];
+        for(String eachWord: words){
+
+            newWords[i] = eachWord.substring(0,1)+eachWord.substring(eachWord.length()-1);
+            i++;
+        }
+        System.out.println(Arrays.toString(newWords));
+    }
+}
